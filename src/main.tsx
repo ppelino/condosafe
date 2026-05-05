714768af05e1f739266dd8c71eda26ec2e139eb7
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2026-05-05 14:15:57 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,27 @@ createRoot(document.getElementById('root')!).render(
   </StrictMode>,
 )
 
-// 🔥 REGISTRO DO SERVICE WORKER (PWA)
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/sw.js')
-      .then(() => {
+      .then((registration) => {
         console.log('Service Worker registrado com sucesso')
+
+        registration.addEventListener('updatefound', () => {
+          const newWorker = registration.installing
+
+          if (newWorker) {
+            newWorker.addEventListener('statechange', () => {
+              if (
+                newWorker.state === 'installed' &&
+                navigator.serviceWorker.controller
+              ) {
+                window.dispatchEvent(new Event('pwa-update-available'))
+              }
+            })
+          }
+        })
       })
       .catch((err) => {
         console.error('Erro ao registrar Service Worker:', err)
